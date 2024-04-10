e1231dfd236413e052037392afd546ab08f152a4
Natalia Lopez <natigab.24@gmail.com>
2024-04-10 18:35:08 -0300

    agregué cors y morgan y realicé la solicitud

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import morgan from 'morgan'
 const app = express();
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto ' + app.get('port'))
+})
+
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get('/', (req, res)=>{
+    console.log('procesando solicitud')
+    res.send('respuesta del backend')
 })
