793bbcd610d8a985d78c4582caae21fe37ba31d7
Natalia Lopez <natigab.24@gmail.com>
2024-04-10 19:32:51 -0300

    configuré un archivo estatico en la ruta principal del backend

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import morgan from 'morgan'
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 
 app.set('port', process.env.PORT || 4000)
@@ -10,9 +12,16 @@ app.listen(app.get('port'), ()=>{
 })
 
 app.use(cors());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//console.log(__filename)
+//console.log(path.join(__dirname,'/public'))
+app.use(express.static(path.join(__dirname,'/public')))
 
-app.get('/', (req, res)=>{
+app.get('/nuevo', (req, res)=>{
     console.log('procesando solicitud')
     res.send('respuesta del backend')
 })
