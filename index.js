6ba245ec9cf9c604d544a1ddbcb3f7cca126652a
Natalia Lopez <natigab.24@gmail.com>
2024-04-11 02:09:44 -0300

    agrego la conexión con la base de datos

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import habitacionesRouter from './src/routes/habitaciones.routes.js'
-
+import './src/database/database.js'
 const app = express();
 
 app.set('port', process.env.PORT || 4000)
