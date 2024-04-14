189a489e25cc604d6b7332a1f6feeb306b1c7b4d
Natalia Lopez <natigab.24@gmail.com>
2024-04-14 02:17:07 -0300

    formateo código

--- src/database/models/usuario.js
+++ src/database/models/usuario.js
@@ -1,54 +1,55 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const usuarioSchema = new Schema({
-    imgUser:{
-        type: String,
-        unique:true
-    },
-    userEmail:{
-        type: String,
-        required: true,
-        minLength: 4,
-        maxLength: 250,
-        validate: {
-            validator:  (valor) => {
-                return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(valor)
-            },
-            message: dato => `${dato.value} no es una dirección de correo valida`
-        },
-        unique:true
-    },
-    userPassword:{
-        type: String,
-        required: true,
-        minLength:8,
-        maxLength:15,
-        validate: {
-            validator:  (valor) => {
-                return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(valor)
-            },
-            message: dato => `${dato.value} no es una contraseña valida`
-        },
-        unique:true
-    },
-   userName:{
+  imgUser: {
     type: String,
-    required:true,
-    minLength:3,
-    maxLength:15,
-    unique:true
-   },
-   nombreUser:{
+    unique: true,
+  },
+  userEmail: {
     type: String,
-    required:true,
-    minLength:3,
-    maxLength:15,
-   },
-   apellidoUser:{
+    required: true,
+    minLength: 4,
+    maxLength: 250,
+    validate: {
+      validator: (valor) => {
+        return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(valor);
+      },
+      message: (dato) => `${dato.value} no es una dirección de correo valida`,
+    },
+    unique: true,
+  },
+  userPassword: {
     type: String,
-    required:true,
-    minLength:3,
-    maxLength:20,
-   },
-   
-})
+    required: true,
+    minLength: 8,
+    maxLength: 15,
+    validate: {
+      validator: (valor) => {
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(valor);
+      },
+      message: (dato) => `${dato.value} no es una contraseña valida`,
+    },
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 15,
+    unique: true,
+  },
+  nombreUser: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 15,
+  },
+  apellidoUser: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+  },
+});
+const Usuario = mongoose.model("usuario", usuarioSchema);
+export default Usuario;
