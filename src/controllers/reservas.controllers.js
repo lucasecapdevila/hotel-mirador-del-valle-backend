df59abe16aec54db1b1cd07c6eacfb0306e0c567
Natalia Lopez <natigab.24@gmail.com>
2024-04-19 17:53:55 -0300

    creo función para crear una reserva

--- src/controllers/reservas.controllers.js
+++ src/controllers/reservas.controllers.js
@@ -1,10 +1,26 @@
 import Reserva from "../database/models/reservas.js";
 export const listarReservas = async (req, res) => {
   try {
-    const reservas = await Reservas.find();
+    const reservas = await Reserva.find();
     res.status(200).json(reservas);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ mensaje: "Error al buscar los usuarios" });
+    res.status(500).json({ mensaje: "Error al buscar los reservas" });
   }
 };
+export const crearReserva = async (req, res) => {
+    try {
+      console.log(req);
+      console.log(req.body);
+      const reservaNueva = new Reserva(req.body);
+      await reservaNueva.save();
+      res.status(201).json({
+        mensaje: "La reserva fue creada con éxito",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        mensaje: "La reserva no se realizó",
+      });
+    }
+  };
