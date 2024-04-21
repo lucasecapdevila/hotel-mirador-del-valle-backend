3db53a27035ed49ef2f2bd70b0d789eb8f9b255f
José Vera <profe.jomanuelvr@gmail.com>
2024-04-21 17:58:24 -0300

    creado el archivo habitaciones js modelo para el back

---
+++ src/database/models/habitaciones.js
@@ -0,0 +1,73 @@
+import mongoose, { Schema } from "mongoose";
+
+const habitacionSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  numeroHabitacion: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  tipoHabitacion: {
+    type: String,
+    required: true,
+    enum: ["Doble", "Deluxe", "Suite", "Twin"],
+  },
+  precioHabitacion: {
+    type: Number,
+    required: true,
+  },
+  reservasActuales: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      idUsuario: {
+        type: String,
+        required: true,
+      },
+      fechaEntrada: {
+        type: String,
+        required: true,
+      },
+      fechaSalida: {
+        type: String,
+        required: true,
+      },
+      precioTotal: {
+        type: Number,
+        required: true,
+      },
+      diasTotales: {
+        type: Number,
+        required: true,
+      },
+    }
+  ],
+  imagenHabitacion: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(value);
+      },
+      message: (dato) => `${dato.value} no es un formato de imagen válido`,
+    },
+    unique: true,
+  },
+  descripcionBreve: {
+    type: String,
+    required: true,
+  },
+  descripcionAmplia: {
+    type: String,
+    required: true,
+  },
+});
+
+const Habitacion = mongoose.model("Habitacion", habitacionSchema);
+export default Habitacion;
